d3273f42dddd1f088542a94b3272eaa50787f894
Martin Lichtman <lichtman@umd.edu>
2019-08-13 22:48:27 -0400

    Lecroy AWG
    The Lecroy AWG has been added and is working.  Not yet using NDSP, still using a bespoke TCP protocol.

--- repository/Lecroy1102_test.py
+++ repository/Lecroy1102_test.py
@@ -14,6 +14,7 @@ import sys
 import time
 from ctypes import *
 
+logger = logging.getLogger(__name__)
 
 class Lecroy1102():
     """Class for programming a Lecroy 1102 AWG"""
@@ -22,61 +23,60 @@ class Lecroy1102():
     max_samples = 2000000  # maximum number of samples to program
     sample_chunk_size = 2  # number of samples must be a multiple of sample_chunk_size
     pad_value = 0  # the waveform will be padded with this number to make it a multiple of sample_chunk_size, or to make it the length of min_samples
-    min_amplitude = 0  # minimum amplitude value (raw)
-    max_amplitude = 1.5  # maximum amplitude value (raw)
+    min_amplitude = -5  # minimum amplitude value (raw)
+    max_amplitude = +5  # maximum amplitude value (raw)
     num_channels = 2  # number of channels
     calibration = lambda voltage: voltage  # function that returns raw amplitude number, given voltage
     calibrationInv = lambda raw: raw #function that returns voltage, given raw amplitude number
 
-    def __enter__(self):
-        print("__")
-        return self.
-
     def __init__(self, IP_address, port, sample_rate, ext_clock_frequency):
+        print('__init__()')
+
         self.enabled = False
         self.IP_address = IP_address
         self.port = port
         self.sample_rate = sample_rate
         self.ext_clock_frequency = ext_clock_frequency
-
-        self.waveforms = []
-        for channel in range(self.deviceProperties['numChannels']):
-            self.waveforms.append(None)
-
         self.sample_length = 1.0/self.sample_rate
-        #new_mag('sample', sample)  # TODO: check whether the added samples = count / second in modules/quantity_units.txt
-        #new_mag('samples', sample)  # replaces this
 
     def send(self, command_string):
+        print('send()')
 
-       		# Send a well formatted message to the server.
-            # The server must be running, and this instance must already have called connect().
-            # Each message begins with MESG,
-            # followed by 4 bytes (a 32 bit unsigned long integer) that give the length of the rest of the message,
-            # followed by the rest of the message (command_string).
+        # Send a well formatted message to the server.
+        # The server must be running, and this instance must already have called connect().
+        # Each message begins with MESG,
+        # followed by 4 bytes (a 32 bit unsigned long integer) that give the length of the rest of the message,
+        # followed by the rest of the message (command_string).
 
-            # find the length of the command_string
-            length = len(command_string)
-            if length>=4294967296: #2**32
-                logging.getLogger(__name__).error('Lecroy AWG TCP message is too long, size = '+str(length)+' bytes')
+        # find the length of the command_string
+        length = len(command_string)
+        if length>=4294967296: #2**32
+            logger.error('Lecroy AWG TCP message is too long, size = '+str(length)+' bytes')
 
-            # format the message
-            message = b'MESG'+struct.pack("!L", length)+command_string
+        # format the message
+        message = b'MESG'+struct.pack("!L", length)+command_string
 
-            # send the message
-            self.sock.sendall(message)
+        # send the message
+        self.sock.sendall(message)
 
     def open(self):
-        logger = logging.getLogger(__name__)
+        print('open()')
+
         try:
 
             # set up TCP connection
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+            self.sock.settimeout(1)
+
+            print('trying connection')
+
             # Open a connection to the TCP server.
             # The server should already be running at the given IP address.
             # The port numbers must agree between the client and server.
             self.sock.connect((self.IP_address, self.port))
+
+            print('connected')
 
             # message format:
             # MESG
@@ -104,76 +104,112 @@ class Lecroy1102():
             self.send(command_string)
 
             self.enabled = True
-            logger.info("Successfully opened {0}".format(self.display_name))
+            print("Successfully opened {0}".format(self.display_name))
         except Exception as e:
             logger.error("Unable to open {0}: {1}".format(self.display_name, e))
             self.enabled = False
         return self.enabled
 
-    def program(self, t0, t1, eq1, eq2):
-        """
-        :param t0: the start time in seconds (start does not delay, but this allows you to change window without modifying equations)
-        :param t1: the end time in seconds
-        :param eq1: a function handle for the first channel waveform.  Must accept a 1D numpy array of times in seconds as the only parameter.
-        :param eq2: a function handle for the first channel waveform.  Must accept a 1D numpy array of times in seconds as the only parameter.
-        :return:
-        """
+    def program(self, waveform1, waveform2):
+        print('program()')
 
-        logger = logging.getLogger(__name__)
         if self.enabled:
-            channel_0_points = self.waveforms[0].evaluate()  # this is a 1D numpy array
-            channel_1_points = self.waveforms[1].evaluate()  # this is a 1D numpy array
-            logger.info("channel_0_points.dtype:"+str(channel_0_points.dtype))
-            logger.info("channel_1_points.dtype:"+str(channel_1_points.dtype))
-            logger.info( "writing {0} points to AWG channel 0".format(len(channel_0_points)) )
-            logger.info( "writing {0} points to AWG channel 1".format(len(channel_1_points)) )
+
+            for waveform in [waveform1, waveform2]:
+                if len(waveform) < self.min_samples:
+                    waveform = np.append(waveform, (self.min_samples - len(waveform)) * [self.pad_value])
+                    logger.warning("Lecroy1102.program(): Sequence is too short.  Padding.")
+                remainder = len(waveform) % self.sample_chunk_size
+                if remainder != 0:
+                    waveform = np.append(waveform, remainder * [self.pad_value])
+                    logger.warning("Lecroy1102.program(): Sequence length is not a multiple of sample_chunk_size.  Padding.")
+                if len(waveform) > self.max_samples:
+                    logger.warning("Lecroy1102.program(): Sequence length exceeds max_samples. Truncating.")
+                if np.amin(waveform) < self.min_amplitude:
+                    waveform[waveform < self.min_amplitude] = self.min_amplitude
+                if np.amax(waveform) > self.max_amplitude:
+                    waveform[waveform > self.max_amplitude] = self.max_amplitude
+
+            print("channel_0_points.dtype:"+str(waveform1.dtype))
+            print("channel_1_points.dtype:"+str(waveform2.dtype))
+            print("writing {0} points to AWG channel 0".format(len(waveform1)) )
+            print("writing {0} points to AWG channel 1".format(len(waveform2)) )
 
             # send waveforms over TCP to Lecroy C# server
 
             command_string = b'UPDA'
-            command_string += struct.pack('!L', len(channel_0_points))
-            command_string += channel_0_points.astype('<f8').tobytes()
-            command_string += struct.pack('!L', len(channel_1_points))
-            command_string += channel_1_points.astype('<f8').tobytes()
+            command_string += struct.pack('!L', len(waveform1))
+            command_string += waveform1.astype('<f8').tobytes()
+            command_string += struct.pack('!L', len(waveform2))
+            command_string += waveform2.astype('<f8').tobytes()
 
             self.send(command_string)
 
+            print('command sent')
+
             time.sleep(1.5)  # wait 1.5 second for AWG programming to complete
+
+            print('sleep done')
 
         else:
             logger.warning("{0} unavailable. Unable to program.".format(self.display_name))
 
     def trigger(self):
-        logger = logging.getLogger(__name__)
+        print('trigger()')
         if self.enabled:
             # SEND COMMAND TO TRIGGER AWG
             self.send('TRIG')
-            logger.info("Triggered {0}".format(self.display_name))
+            print("Triggered {0}".format(self.display_name))
 
     def close(self):
-        logger = logging.getLogger(__name__)
+        print('close()')
         if self.enabled:
             # close the TCP connection to the AWG server
             self.sock.close()
-            logger.info("Connection to {0} closed".format(self.display_name))
+            print("Connection to {0} closed".format(self.display_name))
 
-def eq1(t):
-    return sin(2*np.pi*100*MHZ*t)
-
-def eq2(t)
-    return sin(2*np.pi*100*MHZ*t+np.pi)
 
 class Lecroy1102_test(base_experiment.base_experiment):
 
     def run(self):
+        print('run()')
+
         IP_address = '192.168.1.100'  # server is running on JARVIS
         port = 11000
         sample_rate = 250 * MHz
         ext_clock_frequency = 10 * MHz
-        with awg as Lecroy1102(IP_address, port, sample_rate, ext_clock_frequency):
-        awg.open()
-        t0 = 0
-        t1 = 1*ms
-        awg.program(t0, t1, eq1, eq2)
 
+        try:
+            awg = Lecroy1102(IP_address, port, sample_rate, ext_clock_frequency)
+            awg.open()
+            if not awg.enabled:
+                return
+            print('awg.sample_length', awg.sample_length)
+            t = np.arange(0, 5*us, awg.sample_length)
+            print('length t:', len(t))
+            waveform1 = np.sin(2*np.pi*80*MHz*t)  # +np.sin(2*np.pi*85*MHz*t)
+            waveform2 = np.sin(2*np.pi*85*MHz*t)
+            awg.program(waveform1, waveform2)
+            self.set_dataset('waveforms', np.array([q for q in zip(waveform1, waveform2)]), broadcast=True, persist=True, archive=True)
+            self.set_dataset('waveform1', waveform1, broadcast=True, persist=True, archive=True)
+            self.set_dataset('waveform2', waveform2, broadcast=True, persist=True, archive=True)
+            self.set_dataset('waveform_names', [bytes(i, 'utf-8') for i in ['channel 1', 'channel 2']], broadcast=True, persist=True, archive=True)
+            self.set_dataset('waveform_t', t, broadcast=True, persist=True, archive=True)
+            self.set_dataset('waveform_x_names', [bytes(i, 'utf-8') for i in ['time']], broadcast=True, persist=True, archive=True)
+        finally:
+            awg.close()
+
+        self.kernel_run()
+
+        print('end')
+
+    @kernel
+    def kernel_run(self):
+        self.core.reset()
+        for i in range(1):
+            delay(1 * ms)
+            with parallel:
+                self.ttl8.pulse(1*ms)  # AWG trigger
+                self.ttl9.pulse(1*ms)  # scope trigger
+            delay(1*ms)
 
